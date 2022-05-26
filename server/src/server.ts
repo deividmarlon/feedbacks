292de5f5cc2c29e2c54b54507f059bcb8e0ca11d
Deivid Marlon <deividmarlon@gmail.com>
2022-05-26 00:33:03 -0300

    build added middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
+  console.log("teste")
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
