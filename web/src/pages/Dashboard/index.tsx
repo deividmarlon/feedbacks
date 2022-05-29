d4f92a4247f9ef419e5ebeca7dabbeb9736cffe8
Deivid Marlon <deividmarlon@gmail.com>
2022-05-29 16:43:36 -0300

    changed test from profile to dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,3 +1,26 @@
+import { useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom"
+
 export function Dashboard() {
-  return <h1>Dashboard</h1>
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const foundPaymentId = searchParams.get('payment_id') || '0';
+  const foundStatus = searchParams.get('status') || 'none';
+
+  function isNumeric(value:string){
+      const test = /^-?\d+$/.test(value);
+      return test;      
+  }
+
+  useEffect(()=>{
+    if(isNumeric(foundPaymentId)){
+      console.log(foundPaymentId)
+    }else{
+      console.log('treta')
+    }
+    console.log(foundStatus)
+  },[searchParams])
+
+  return (<h1>Dashboard: {foundPaymentId}, age: {foundStatus}</h1>)
 }
